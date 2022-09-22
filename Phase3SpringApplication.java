d75149ae3fe6084675bb29089d4a11ce47e331ec
thirukannan88 <109619398+thirukannan88@users.noreply.github.com>
2022-09-22 11:53:53 +0530

    Add files via upload

---
+++ Phase3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Phase3_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3SpringApplication.class, args);
+	}
+
+}
